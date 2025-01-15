844534ee276c6856aa38e0508b1a85172efb0ff3
marcuslannister <marcus@lannister.cc>
2025-01-15 19:48:50 +0800

    feat: disable cursor animation

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,10 @@
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
+--
+
+if vim.g.neovide then
+  vim.g.neovide_cursor_animation_length = 0
+end
 
 vim.opt.wrapscan = false -- Prevent search from wrapping
