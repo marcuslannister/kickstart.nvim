ffd9392632db26c2f1b5cd34d60854b0829412c0
marcuslannister <marcus@lannister.cc>
2025-05-06 22:22:48 +0800

    feat: change color to modus

--- init.lua
+++ init.lua
@@ -835,54 +835,88 @@ require('lazy').setup({
   --   end,
   -- },
 
+  -- {
+  --   'loctvl842/monokai-pro.nvim',
+  --   config = function()
+  --     require('monokai-pro').setup {
+  --       transparent_background = false,
+  --       terminal_colors = true,
+  --       devicons = true, -- highlight the icons of `nvim-web-devicons`
+  --       styles = {
+  --         comment = { italic = true },
+  --         keyword = { italic = true }, -- any other keyword
+  --         type = { italic = true }, -- (preferred) int, long, char, etc
+  --         storageclass = { italic = true }, -- static, register, volatile, etc
+  --         structure = { italic = true }, -- struct, union, enum, etc
+  --         parameter = { italic = true }, -- parameter pass in function
+  --         annotation = { italic = true },
+  --         tag_attribute = { italic = true }, -- attribute of tag in reactjs
+  --       },
+  --       filter = 'pro', -- classic | octagon | pro | machine | ristretto | spectrum
+  --       -- Enable this will disable filter option
+  --       day_night = {
+  --         enable = false, -- turn off by default
+  --         day_filter = 'pro', -- classic | octagon | pro | machine | ristretto | spectrum
+  --         night_filter = 'spectrum', -- classic | octagon | pro | machine | ristretto | spectrum
+  --       },
+  --       inc_search = 'background', -- underline | background
+  --       background_clear = {
+  --         -- "float_win",
+  --         'toggleterm',
+  --         'telescope',
+  --         -- "which-key",
+  --         'renamer',
+  --         'notify',
+  --         -- "nvim-tree",
+  --         -- "neo-tree",
+  --         -- "bufferline", -- better used if background of `neo-tree` or `nvim-tree` is cleared
+  --       }, -- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree", "nvim-tree", "bufferline"
+  --       plugins = {
+  --         bufferline = {
+  --           underline_selected = false,
+  --           underline_visible = false,
+  --         },
+  --         indent_blankline = {
+  --           context_highlight = 'default', -- default | pro
+  --           context_start_underline = false,
+  --         },
+  --       },
+  --     }
+  --     vim.cmd [[colorscheme monokai-pro]]
+  --   end,
+  -- },
+
   {
-    'loctvl842/monokai-pro.nvim',
+    'miikanissi/modus-themes.nvim',
     config = function()
-      require('monokai-pro').setup {
-        transparent_background = false,
-        terminal_colors = true,
-        devicons = true, -- highlight the icons of `nvim-web-devicons`
+      require('modus-themes').setup {
+        -- Theme comes in two styles `modus_operandi` and `modus_vivendi`
+        -- `auto` will automatically set style based on background set with vim.o.background
+        style = 'auto',
+        variant = 'tinted', -- Theme comes in four variants `default`, `tinted`, `deuteranopia`, and `tritanopia`
+        transparent = false, -- Transparent background (as supported by the terminal)
+        dim_inactive = false, -- "non-current" windows are dimmed
         styles = {
-          comment = { italic = true },
-          keyword = { italic = true }, -- any other keyword
-          type = { italic = true }, -- (preferred) int, long, char, etc
-          storageclass = { italic = true }, -- static, register, volatile, etc
-          structure = { italic = true }, -- struct, union, enum, etc
-          parameter = { italic = true }, -- parameter pass in function
-          annotation = { italic = true },
-          tag_attribute = { italic = true }, -- attribute of tag in reactjs
+          -- Style to be applied to different syntax groups
+          -- Value is any valid attr-list value for `:help nvim_set_hl`
+          comments = { italic = false },
+          keywords = { italic = false },
+          functions = {},
+          variables = {},
         },
-        filter = 'pro', -- classic | octagon | pro | machine | ristretto | spectrum
-        -- Enable this will disable filter option
-        day_night = {
-          enable = false, -- turn off by default
-          day_filter = 'pro', -- classic | octagon | pro | machine | ristretto | spectrum
-          night_filter = 'spectrum', -- classic | octagon | pro | machine | ristretto | spectrum
-        },
-        inc_search = 'background', -- underline | background
-        background_clear = {
-          -- "float_win",
-          'toggleterm',
-          'telescope',
-          -- "which-key",
-          'renamer',
-          'notify',
-          -- "nvim-tree",
-          -- "neo-tree",
-          -- "bufferline", -- better used if background of `neo-tree` or `nvim-tree` is cleared
-        }, -- "float_win", "toggleterm", "telescope", "which-key", "renamer", "neo-tree", "nvim-tree", "bufferline"
-        plugins = {
-          bufferline = {
-            underline_selected = false,
-            underline_visible = false,
-          },
-          indent_blankline = {
-            context_highlight = 'default', -- default | pro
-            context_start_underline = false,
-          },
-        },
+
+        --- You can override specific color groups to use other groups or a hex color
+        --- function will be called with a ColorScheme table
+        ---@param colors ColorScheme
+        on_colors = function(colors) end,
+
+        --- You can override specific highlights to use other groups or a hex color
+        --- function will be called with a Highlights and ColorScheme table
+        ---@param highlights Highlights
+        ---@param colors ColorScheme
+        on_highlights = function(highlights, colors) end,
       }
-      vim.cmd [[colorscheme monokai-pro]]
+      vim.cmd [[colorscheme modus]] -- modus_operandi, modus_vivendi
     end,
   },
 
