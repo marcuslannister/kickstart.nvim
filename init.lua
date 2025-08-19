c41193c6dc99593d57df33b98dfb5dd28d43a965
marcuslannister <marcus@lannister.cc>
2025-08-19 19:40:50 +0800

    feat: change color to flexoki

--- init.lua
+++ init.lua
@@ -886,37 +886,45 @@ require('lazy').setup({
   --   end,
   -- },
 
+  -- {
+  --   'miikanissi/modus-themes.nvim',
+  --   config = function()
+  --     require('modus-themes').setup {
+  --       -- Theme comes in two styles `modus_operandi` and `modus_vivendi`
+  --       -- `auto` will automatically set style based on background set with vim.o.background
+  --       style = 'auto',
+  --       variant = 'tinted', -- Theme comes in four variants `default`, `tinted`, `deuteranopia`, and `tritanopia`
+  --       transparent = false, -- Transparent background (as supported by the terminal)
+  --       dim_inactive = false, -- "non-current" windows are dimmed
+  --       styles = {
+  --         -- Style to be applied to different syntax groups
+  --         -- Value is any valid attr-list value for `:help nvim_set_hl`
+  --         comments = { italic = false },
+  --         keywords = { italic = false },
+  --         functions = {},
+  --         variables = {},
+  --       },
+  --
+  --       --- You can override specific color groups to use other groups or a hex color
+  --       --- function will be called with a ColorScheme table
+  --       ---@param colors ColorScheme
+  --       on_colors = function(colors) end,
+  --
+  --       --- You can override specific highlights to use other groups or a hex color
+  --       --- function will be called with a Highlights and ColorScheme table
+  --       ---@param highlights Highlights
+  --       ---@param colors ColorScheme
+  --       on_highlights = function(highlights, colors) end,
+  --     }
+  --     vim.cmd [[colorscheme modus]] -- modus_operandi, modus_vivendi
+  --   end,
+  -- },
+
   {
-    'miikanissi/modus-themes.nvim',
+    'nuvic/flexoki-neovim',
     config = function()
-      require('modus-themes').setup {
-        -- Theme comes in two styles `modus_operandi` and `modus_vivendi`
-        -- `auto` will automatically set style based on background set with vim.o.background
-        style = 'auto',
-        variant = 'tinted', -- Theme comes in four variants `default`, `tinted`, `deuteranopia`, and `tritanopia`
-        transparent = false, -- Transparent background (as supported by the terminal)
-        dim_inactive = false, -- "non-current" windows are dimmed
-        styles = {
-          -- Style to be applied to different syntax groups
-          -- Value is any valid attr-list value for `:help nvim_set_hl`
-          comments = { italic = false },
-          keywords = { italic = false },
-          functions = {},
-          variables = {},
-        },
-
-        --- You can override specific color groups to use other groups or a hex color
-        --- function will be called with a ColorScheme table
-        ---@param colors ColorScheme
-        on_colors = function(colors) end,
-
-        --- You can override specific highlights to use other groups or a hex color
-        --- function will be called with a Highlights and ColorScheme table
-        ---@param highlights Highlights
-        ---@param colors ColorScheme
-        on_highlights = function(highlights, colors) end,
-      }
-      vim.cmd [[colorscheme modus]] -- modus_operandi, modus_vivendi
+      -- Set colorscheme aftr options
+      vim.cmd [[colorscheme flexoki]]
     end,
   },
 
